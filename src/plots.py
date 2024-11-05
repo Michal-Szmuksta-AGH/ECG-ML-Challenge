e1e823858e0d83a831408301b52a147fffc957a7
Baey <blazejszargut@gmail.com>
2024-11-05 18:13:17 +0000

    Dodane t_start i t_end

--- src/plots.py
+++ src/plots.py
@@ -3,26 +3,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 from typing import Union, Iterable
 
-def plot_ecg(record: Union[np.ndarray, wfdb.Record], fs: int, channels: Union[Iterable[int], int] = -1, duration: int = 10) -> plt.Figure:
+
+def plot_ecg(
+    record: Union[np.ndarray, wfdb.Record],
+    fs: int,
+    channels: Union[Iterable[int], int] = -1,
+    t_start: Union[float, int] = 0,
+    t_end: Union[float, int] = None,
+) -> plt.Figure:
     """
     Plot ECG signal.
 
     :param record: Numpy array ECG signal or wfdb.Record object.
     :param fs: Sampling frequency.
-    :param channels: List of channels to plot.
-    :param duration: Duration of the signal to plot.
+    :param channels: List of channels to plot. If -1, plot all channels.
+    :param t_start: Start time of the plot.
+    :param t_end: End time of the plot. If None, plot the whole signal.
     """
     if not isinstance(record, (np.ndarray, wfdb.Record)):
-        raise ValueError('record must be a numpy array or wfdb.Record object')
+        raise ValueError("record must be a numpy array or wfdb.Record object")
     if not isinstance(fs, int):
-        raise ValueError('fs must be an integer')
-    
+        raise ValueError("fs must be an integer")
+    if not isinstance(channels, (Iterable, int)):
+        raise ValueError("channels must be an iterable or integer")
+    if not isinstance(t_start, (float, int)):
+        raise ValueError("t_start must be a float or integer")
+    if t_end is not None and not isinstance(t_end, (float, int)):
+        raise ValueError("t_end must be a float or integer")
+
     if isinstance(record, wfdb.Record):
         signal = record.p_signal
     else:
         signal = record
 
-    time = np.linspace(0, duration, duration * fs)
+    if t_end is None:
+        t_end = signal.shape[0] / fs
+
+    time = np.linspace(t_start, t_end, (t_end - t_start) * fs)
     if channels == -1:
         try:
             channels = np.arange(signal.shape[1])
@@ -31,23 +48,27 @@ def plot_ecg(record: Union[np.ndarray, wfdb.Record], fs: int, channels: Union[It
         num_channels = len(channels)
     else:
         num_channels = len(channels)
-    
+
     sig_name = record.sig_name if isinstance(record, wfdb.Record) else None
-    
-    fig, ax = plt.subplots(num_channels, 1, figsize=(10, 4 * num_channels), sharex=True, sharey=True)
+
+    fig, ax = plt.subplots(
+        num_channels, 1, figsize=(10, 4 * num_channels), sharex=True, sharey=True
+    )
     if num_channels == 1:
         ax = [ax]
         signal = signal.reshape(-1, 1)
-    fig.suptitle('ECG signal')
+    fig.suptitle("ECG signal")
     fig.patch.set_facecolor("#ffe7e7")
     for i, channel in enumerate(channels):
-        ax[i].plot(time, signal[:duration * fs, channel], color='blue')
-        ax[i].set_ylabel('Amplitude')
+        ax[i].plot(time, signal[t_start * fs : t_end * fs, channel], color="blue")
+        ax[i].set_ylabel("Amplitude")
+        ax[i].set_xlim(t_start, t_end)  # Set x-axis limits
+        ax[i].set_xticks(np.linspace(t_start, t_end, num=10))
         ax[i].set_title(f"CH{channel}{': ' + sig_name[channel] if sig_name is not None else ''}")
         ax[i].set_facecolor("#ffe7e7")
-        ax[i].grid(color='red', linewidth=0.5)
+        ax[i].grid(color="red", linewidth=0.5)
 
-    ax[-1].set_xlabel('Time [s]')
+    ax[-1].set_xlabel("Time [s]")
     fig.tight_layout(rect=[0, 0.03, 1, 0.95])
 
     return fig
--- test/plots_test.py
+++ test/plots_test.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from src import plot_ecg
 
-TEST_DIR = 'tmp'
-TEST_DATASET_DIR = 'mit-bih-arrhythmia-database'
+TEST_DIR = "tmp"
+TEST_DATASET_DIR = "mit-bih-arrhythmia-database"
 
 
 class TestPlots(unittest.TestCase):
@@ -21,41 +21,45 @@ class TestPlots(unittest.TestCase):
         Test record type error.
         """
         with self.assertRaises(ValueError):
-            plot_ecg('record', 360)
+            plot_ecg("record", 360)
 
     def test_fs_type_error(self):
         """
         Test fs type error.
         """
         with self.assertRaises(ValueError):
-            plot_ecg([1, 2, 3], 'fs')
+            plot_ecg([1, 2, 3], "fs")
 
     def test_channels_type_error(self):
         """
         Test channels type error.
         """
         with self.assertRaises(ValueError):
-            plot_ecg([1, 2, 3], 360, channels='channels')
-    
+            plot_ecg([1, 2, 3], 360, channels="channels")
+
     def test_plot_ecg(self):
         """
         Test plot_ecg function.
         """
         os.chdir(os.path.dirname(os.path.abspath(__file__)))
-        record = wfdb.rdrecord(os.path.join(TEST_DIR, TEST_DATASET_DIR, '103'))
-        fig = plot_ecg(record.p_signal, record.fs, duration=10)
+        record = wfdb.rdrecord(os.path.join(TEST_DIR, TEST_DATASET_DIR, "103"))
+        fig = plot_ecg(record.p_signal, record.fs, t_start=10, t_end=25)
         self.assertIsInstance(fig, plt.Figure)
-        plt.savefig('tmp/test_plot_ecg.png')
+        plt.savefig("tmp/test_plot_ecg.png")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     if not os.path.exists(TEST_DIR):
         os.makedirs(TEST_DIR)
 
-    if not os.path.exists(TEST_DATASET_DIR) or len(os.listdir(os.path.join(TEST_DIR, TEST_DATASET_DIR))) == 0:
+    if (
+        not os.path.exists(TEST_DATASET_DIR)
+        or len(os.listdir(os.path.join(TEST_DIR, TEST_DATASET_DIR))) == 0
+    ):
         print("Downloading database...")
-        wfdb.dl_database('mitdb', dl_dir=os.path.join(TEST_DIR, TEST_DATASET_DIR))
+        wfdb.dl_database("mitdb", dl_dir=os.path.join(TEST_DIR, TEST_DATASET_DIR))
     else:
         print("Database already downloaded!")
 
