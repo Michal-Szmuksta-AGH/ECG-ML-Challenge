cebe51d1366d2d4710fb5b8f6ac64f8b407301c4
Błażej <blazejszargut@gmail.com>
2024-11-08 19:43:12 +0000

    Initial upload of GRU model training

--- src/utilities/dataloader.py
+++ src/utilities/dataloader.py
@@ -20,6 +20,8 @@ class ECGDataset(Dataset):
         self.input_dtype: torch.dtype = input_dtype
         self.output_dtype: torch.dtype = output_dtype
         self.files: List[str] = [f for f in os.listdir(directory) if f.endswith('.npz')]
+        if not self.files:
+            raise ValueError(f"No .npz files found in directory: {directory}")
 
     def __len__(self):
         return len(self.files)
@@ -33,23 +35,19 @@ class ECGDataset(Dataset):
         y = torch.tensor(y, dtype=self.output_dtype)
         return x, y
 
-# def create_dataloader(directory, batch_size=32, shuffle=True, num_workers=4):
-#     dataset = ECGDataset(directory)
-#     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
-#     return dataloader
-
 class ECGDataLoader(DataLoader):
     """
     DataLoader class for ECG data
     """
 
     def __init__(
-            self, directory: str,
+            self,
+            directory: str,
             input_dtype: torch.dtype = torch.float32,
             output_dtype: torch.dtype = torch.uint8,
             batch_size: int = 32,
             shuffle: bool = True, 
-            num_workers: int = 4):
+            num_workers: int = 1):
         """
         ECGDataLoader constructor
         :param directory: Directory containing the ECG data files
@@ -64,6 +62,8 @@ class ECGDataLoader(DataLoader):
             input_dtype=input_dtype,
             output_dtype=output_dtype
             )
+        if len(dataset) == 0:
+            raise ValueError(f"No data found in directory: {directory}")
         super().__init__(
             dataset,
             batch_size=batch_size,
